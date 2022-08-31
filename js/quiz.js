3e18a67e1cca867f909fdca6753257c1c0460594
Warona Lekoba <wlekoba@yahoo.com>
2022-08-31 18:35:30 +0200

    Final Commit

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"How many legs does a spider have?",
+        option:[
+            "1.two",
+            "2.eight",
+            "3.five",
+           "4.seven",
+        ],
+        answer:2,
+    },
+    {
+        question:"What is something you hit with a hammer?",
+        option:[
+            "1.nail",
+            "2.head",
+            "3.tyre",
+            "4.ball",  
+           
+        ],
+        answer:1,
+    },
+    {
+        question:"If you freeze water, what do you get?",
+        option:[
+            "1.frozen water",
+            "2.nothing",
+            "3.a rock",
+           "4.ice",   
+        ],
+        answer:4,
+    },
+    {
+        question:"How many planets are there in our solar system?",
+        option:[
+            "1.four",
+            "2.eight",
+            "3.ten",
+            "4.six",   
+        ],
+        answer:2,
+
+    },
+    {
+        question:"How many pairs of wings do bees have?",
+        option:[
+            "1.one",
+            "2.two",
+            "3.four",
+            "4.six",   
+        ],
+        answer:2,
+    }
+]
